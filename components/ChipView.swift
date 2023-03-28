f80f37c363540aee326d43732758eaf65258c243
soc06212 <soc06212@gmail.com>
2023-03-28 15:32:01 +0900

    Feat: RoundedCornerView, ChipView 추가

---
+++ components/ChipView.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by 최명근 on 2023/03/28.
+//
+
+import Foundation
+import SwiftUI
+
+struct ChipView<Content>: View where Content: View {
+    @ViewBuilder var content: () -> Content
+    
+    var body: some View {
+        HStack {
+            content()
+        }.padding(EdgeInsets(top: 6, leading: 16, bottom: 6, trailing: 16))
+            .background(
+                ZStack {
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: geometry.size.height / 2)
+                            .foregroundColor(Color("ColorBgPrimary"))
+                            .shadow(color: Color.black.opacity(0.2), radius: 2, y: 2)
+                        RoundedRectangle(cornerRadius: geometry.size.height / 2)
+                            .strokeBorder(Color("ColorPrimary"), style: StrokeStyle(lineWidth: 1))
+                            .foregroundColor(Color.clear)
+                    }
+                }
+            )
+    }
+}
---
+++ components/RoundedCornerView.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  
+//
+//  Created by 최명근 on 2023/03/28.
+//
+
+import Foundation
+import SwiftUI
+
+// MARK: - RoundedCornerView
+struct RoundedCornerView<Content>: View where Content: View {
+    var backgroundColor: Color = Color("ColorBgSecondary")
+    var cornerRadius: CGFloat = 8.0
+    var shadowColor: Color = Color.black.opacity(0.3)
+    var shadowRadius: CGFloat = 0
+    var corners: UIRectCorner? = nil
+    
+    @ViewBuilder var content: () -> Content
+    
+    var body: some View {
+        HStack {
+            content()
+        }.padding(EdgeInsets(top: 12, leading: 8, bottom: 12, trailing: 8))
+            .background(
+                Rectangle()
+                    .fill(backgroundColor)
+                    .cornerRadius(cornerRadius, corners: corners != nil ? corners! : .allCorners)
+                    .shadow(color: shadowColor, radius: shadowRadius, y: shadowRadius == 0 ? 0 : shadowRadius / 2)
+            )
+    }
+    
+}
